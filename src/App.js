11e50fa6183bc73f304c7b19f91e045f84c43aed
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2020-07-19 23:35:50 -0300

    aaa

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [participation, setParticipation] = useState(0);
+
+  
+
+  const handlePartner = (e) => {
+    e.preventDefault();
+    // 1 >> pegar os dados fN, lN, parti
+    // 2 >> colocar todas participações num acumulador
+    // 3 >> fazer verificação se o acumulador está menor que 100
+    // 4 >> try catch
+    //const data = {firstName, lastName, participation};
+    //setCount(participation);
+
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="header">
+        <form onSubmit={handlePartner}>
+              <input type="text"  placeholder="First Name" value={firstName} onChange={e => setFirstName(e.target.value)}/>
+              <input type="text" placeholder="Last Name" value={lastName} onChange={e => setLastName(e.target.value)}/>
+              <input type="number" placeholder="Participation" value={participation} onChange={e => setParticipation(e.target.value)}/>
+              <button type="submit">SEND</button>
+        </form>
+      </div>
+      <div className="data">
+        <h1>DATA</h1>
+        <p>Lorem ipsur dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
